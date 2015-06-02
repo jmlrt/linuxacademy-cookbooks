ba92d487547dbd1b7251effa6291da9832c097db
jmlrt <git@mailleret.fr>
2015-06-03 00:51:38 +0200

    fix typo in platform attribute

--- cookbooks/apache/attributes/default.rb
+++ cookbooks/apache/attributes/default.rb
@@ -2,7 +2,7 @@ default["apache"]["sites"]["jmlrt2"] = { "site_title" => "JMLRT2's website is co
 default["apache"]["sites"]["jmlrt2b"] = { "site_title" => "JMLRT2b's website is coming soon", "port" => 80, "domain" => "jmlrt2b.mylabserver.com" }
 default["apache"]["sites"]["jmlrt3"] = { "site_title" => "jmlrt3 website", "port" => 80, "domain" => "jmlrt3.mylabserver.com" }
 
-case node["plateform"]
+case node["platform"]
 when "centos"
 	default["apache"]["package"] = "httpd"
 when "ubuntu"

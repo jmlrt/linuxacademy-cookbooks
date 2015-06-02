fcbe029429cdc3c97d096000b44200e30114c242
jmlrt <git@mailleret.fr>
2015-06-02 08:10:09 +0200

    Building The Web Server Cookbook: Adding Platform Support To The Cookbook

---
+++ cookbooks/apache/attributes/default.rb
@@ -0,0 +1,10 @@
+default["apache"]["sites"]["jmlrt2"] = { "site_title" => "JMLRT2's website is coming soon", "port" => 80, "domain" => "jmlrt2.mylabserver.com" }
+default["apache"]["sites"]["jmlrt2b"] = { "site_title" => "JMLRT2b's website is coming soon", "port" => 80, "domain" => "jmlrt2b.mylabserver.com" }
+default["apache"]["sites"]["jmlrt3"] = { "site_title" => "jmlrt3 website", "port" => 80, "domain" => "jmlrt3.mylabserver.com" }
+
+case node["plateform"]
+when "centos"
+	default["apache"]["package"] = "httpd"
+when "ubuntu"
+	default["apache"]["package"] = "apache2"
+end

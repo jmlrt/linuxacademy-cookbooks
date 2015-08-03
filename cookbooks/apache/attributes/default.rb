1fd8d79452a68b7d7218c6a8c373401dc55705b4
jmlrt <git@mailleret.fr>
2015-08-03 14:38:08 +0200

    Creating A Second Version Of Our WebServer Cookbook

--- cookbooks/apache/attributes/default.rb
+++ cookbooks/apache/attributes/default.rb
@@ -1,6 +1,7 @@
 default["apache"]["sites"]["jmlrt2"] = { "site_title" => "JMLRT2's website is coming soon", "port" => 80, "domain" => "jmlrt2.mylabserver.com" }
 default["apache"]["sites"]["jmlrt2b"] = { "site_title" => "JMLRT2b's website is coming soon", "port" => 80, "domain" => "jmlrt2b.mylabserver.com" }
 default["apache"]["sites"]["jmlrt3"] = { "site_title" => "jmlrt3 website", "port" => 80, "domain" => "jmlrt3.mylabserver.com" }
+default["author"]["name"] = "jmlrt"
 
 case node["platform"]
 when "centos"
